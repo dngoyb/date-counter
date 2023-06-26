9530ceb94cc09a1ec6a653c4cdf5cc685c941dee
Ngoy <dngoyb@gmail.com>
2023-06-26 15:30:30 +0200

    Step counting done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from 'react';
+
 function App() {
+	const [step, setStep] = useState(1);
+
+	const handleStepMinus = () => {
+		setStep((prev) => step - 1);
+	};
+	const handleStepPlus = () => {
+		setStep((prev) => step + 1);
+	};
 	return (
 		<>
-			<div>hello</div>
+			<div>
+				<button onClick={handleStepMinus}>-</button>
+				<span>Step: {step}</span>
+				<button onClick={handleStepPlus}>+</button>
+			</div>
+			<div>
+				<button>-</button>
+				<span>counter: 0</span>
+				<button>+</button>
+			</div>
+			<p>Today is Monday Jun 26 2023</p>
 		</>
 	);
 }
